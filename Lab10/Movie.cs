525e9fc31b09a9247b986719c046e227b701101c
famouschamus <chamusg@gmail.com>
2018-02-02 16:20:28 -0500

    initial commit

---
+++ Lab10/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+namespace Lab10
+
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public Movie(string title, string category)
+        {
+            this.title = title;
+            this.category = category;
+            System.Console.WriteLine(title + " " + category);
+
+
+        }
+        public static void PresentList(List<Movie> action, List<Movie> sciFi, List<Movie> animation, List<Movie> comedy)
+        {
+
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> movieTitles = new List<string> { "Indiana Jones", "Star Wars", "Toy Story", "Monsters Inc.", "Alien", "Sahara", "Happy Gilmore", "The Wedding Singer", "Yellow Submarine", "The Lego Movie" };
+            List<string> movieCategory = new List<string> { "Action/Adventure", "Sci-Fi", "Animation", "Animation", "Sci-Fi", "Action/Adventure", "Comedy", "Comedy", "Animation", "Animation" };
+            Movie[] movie = new Movie[10];
+            List<Movie> listOfMovie = new List<Movie>();
+            List<Movie> listOfAction = new List<Movie>();
+            List<Movie> listOfSciFi = new List<Movie>();
+            List<Movie> listOfAnimation = new List<Movie>();
+            List<Movie> listOfComedy = new List<Movie>();
+
+            for (int i = 0; i < movieTitles.Count; i++)
+            {
+                movie[i] = new Movie(movieTitles.ElementAt(i), movieCategory.ElementAt(i));
+                listOfMovie.Add(movie[i]);
+
+                if (movieCategory.ElementAt(i) == "Action/Adventure")
+                {
+                    listOfAction.Add(movie[i]);
+                }
+                else if (movieCategory.ElementAt(i) == "Sci-Fi")
+                {
+                    listOfSciFi.Add(movie[i]);
+                }
+                else if (movieCategory.ElementAt(i) == "Animation")
+                {
+                    listOfAnimation.Add(movie[i]);
+                }
+                else
+                {
+                    listOfComedy.Add(movie[i]);
+                }
+            }
+
+
+
+        }
+    }
+}
